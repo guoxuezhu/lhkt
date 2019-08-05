a3f4bfb5544e8b36be1968b95ac308842b5e39ef
gxz <1174327885@qq.com>
2019-08-05 14:44:17 +0800

    fix

--- app/src/main/java/com/hzlh/lhkt/ui/SplashActivity.java
+++ app/src/main/java/com/hzlh/lhkt/ui/SplashActivity.java
@@ -1,8 +1,12 @@
 package com.hzlh.lhkt.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.hzlh.lhkt.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends BaseActivity {
 
@@ -10,5 +14,15 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },2000);
+
     }
 }
