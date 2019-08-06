84683e64cf9d147fc6ef001f6dc058b1caaf4ea7
gxz <1174327885@qq.com>
2019-08-06 10:03:30 +0800

    fix UI

--- app/src/main/java/com/hzlh/lhkt/ui/fragment/MyFragment.java
+++ app/src/main/java/com/hzlh/lhkt/ui/fragment/MyFragment.java
@@ -7,11 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.hzlh.lhkt.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MyFragment extends Fragment {
+
+    @BindView(R.id.user_img)
+    CircleImageView user_img;
+
 
 
     @Nullable
@@ -19,7 +26,19 @@ public class MyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.my_fragment, container, false);
         ButterKnife.bind(this, view);
+        init();
         return view;
+
+    }
+
+    private void init() {
+        Glide.with(MyFragment.this)
+                .load(R.drawable.main_bg)
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+                .centerCrop()
+//                .transform(new CenterCrop(getContext()), new GlideRoundTransform(MyFragment.this, 2))
+                .into(user_img);
 
     }
 
