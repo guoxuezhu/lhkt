bafb2e1197f75fe35bc9b72a291678c5781904d2
gxz <1174327885@qq.com>
2019-08-07 17:06:25 +0800

    fix UI

---
+++ app/src/main/java/com/hzlh/lhkt/ui/LuzhiVideoActivity.java
@@ -0,0 +1,22 @@
+package com.hzlh.lhkt.ui;
+
+import android.os.Bundle;
+
+import com.hzlh.lhkt.R;
+
+import butterknife.ButterKnife;
+
+public class LuzhiVideoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luzhi_video);
+        ButterKnife.bind(this);
+
+
+    }
+
+
+
+}
